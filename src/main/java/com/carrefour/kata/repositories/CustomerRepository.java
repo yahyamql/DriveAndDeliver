289500ff09c91abeca89a52e8e0494910c190d8f
yahyamql <shiniga.yox@gmail.com>
2024-08-03 18:31:36 +0100

    Adding unit tests

--- src/main/java/com/carrefour/kata/repositories/CustomerRepository.java
+++ src/main/java/com/carrefour/kata/repositories/CustomerRepository.java
@@ -1,11 +1,7 @@
 package com.carrefour.kata.repositories;
 
 import com.carrefour.kata.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 public interface CustomerRepository extends ReactiveCrudRepository<Customer, Long> {
 }
